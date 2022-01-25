f9013006cf8bf857a83baf4c6efb392b0daedfa3
Steev <mauricefl@outlook.de>
2022-01-25 20:42:02 +0100

    supportive commit supportive power was brought in to help speedingup development in order to do so history and comments component was setup properly and

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HistoryandcommentsComponent } from './historyandcomments/historyandcomments.component';
 import { StatsComponent } from './stats/stats.component';
 import { TaskbarComponent } from './taskbar/taskbar.component';
+import { CommentsComponent } from './comments/comments.component';
+import { HistoryComponent } from './history/history.component';
+
+const routes: Routes = [
+  { path: 'stats', component: StatsComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HistoryandcommentsComponent,
     StatsComponent,
-    TaskbarComponent
+    TaskbarComponent,
+    CommentsComponent,
+    HistoryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
